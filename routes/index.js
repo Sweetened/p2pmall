2ce7da895b9ffef7cc4ad03fa6cbc23a4845791e
zhoutaoo <zhoutaoo@foxmail.com>
2014-07-14 22:37:36 +0800

    add a index page

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: '借贷商城' });
 });
 
 module.exports = router;
