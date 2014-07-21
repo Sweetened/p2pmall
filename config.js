494a820d54cc1b356ec3e7e343d740f1920050fc
zhoutaoo <zhoutaoo@foxmail.com>
2014-07-21 23:12:12 +0800

    mongodb dep

---
+++ config.js
@@ -0,0 +1,12 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: c-sailor.zhang
+ * Date: 1/23/13
+ * Time: 2:24 PM
+ * To change this template use File | Settings | File Templates.
+ */
+module.exports={
+    cookieSecret:'njblog',
+    connectionstring:'mongodb://njblog:njblog@linus.mongohq.com:10062/NJBlog'
+    // connectionstring:'mongodb://localhost/njblog'
+};
