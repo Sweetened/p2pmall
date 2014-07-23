252447c4c7a6d468840ace388cbef287b272bd6e
zhoutaoo <zhoutaoo@foxmail.com>
2014-07-24 00:02:21 +0800

    index page hot loans get data from mongodb

--- public/javascripts/index/index.js
+++ public/javascripts/index/index.js
@@ -1,15 +1,11 @@
 var app = angular.module('myApp', []);
 app.controller('hotController', function ($scope, $http, $location) {
-    $scope.hotLoans = [
-        {name: '富盈人生第1期', company: 'lufax', time: 24, description: '18%', amount: 3000},
-        {name: '天天盈', company: 'renrendai', time: 36, description: '19%', amount: 600000},
-        {name: '半年盈', company: 'hepandai', time: 6, description: '14%', amount: 1000},
-        {name: '年年盈', company: 'lufax', time: 12, description: '10%', amount: 120000},
-        {name: '季季盈', company: 'yooli', time: 3, description: '17.8%', amount: 30000}
-    ];
-    var investsUrl = "http://" + $location.host() + ":" + $location.port() + '/rest/invests';
-    $http.get(investsUrl).success(function (data) {
+    var baseUrl = "http://" + $location.host() + ":" + $location.port();
+    $http.get(baseUrl + '/rest/invests').success(function (data) {
         $scope.hotInvests = data;
+    });
+    $http.get(baseUrl + '/rest/loans').success(function (data) {
+        $scope.hotLoans = data;
     });
 
 });
