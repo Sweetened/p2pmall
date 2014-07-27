3eba207ae398c652750d91bcd5769b0245e6e431
zhoutaoo <zhoutaoo@foxmail.com>
2014-07-27 21:52:53 +0800

    fix css

--- routes/company.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req, res) {
-  res.render('company', { title: '行业宝典' });
-});
-
-module.exports = router;
