d3b047cdae8fd0f99ccd66f606d275da0f7ecdec
skchang0127 <skchang0127@gmail.com>
2014-07-30 22:43:07 +0800

    modify search page select field

--- app/scripts/index/invest.js
+++ app/scripts/index/invest.js
@@ -2,7 +2,16 @@
 
 app.controller('investController', function ($scope, $http, $location) {
     var baseUrl = "http://" + $location.host() + ":" + $location.port();
-    $http({url: baseUrl + "/rest/search/invests", data: user}).success(function (data, status, headers, config) {
-        $scope.invests = data;
-    });
+    
+    $scope.checked = function (condition,value){
+    	$("#invest-list-filter ."+condition).find(".active").removeClass("active");
+    	$("#invest-list-filter ."+condition).find("."+value).addClass("active");
+    	$scope[condition] = value;
+    	$scope.$watch(condition,function(value){
+    		$http({url: baseUrl + "/rest/search/invests"})
+    		.success(function (data, status, headers, config) {
+        			$scope.invests = data;
+    		});
+    	});
+    }
 });
